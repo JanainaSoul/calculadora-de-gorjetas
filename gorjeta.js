0d0702ae565a787f9ca6e2fc2ee0e85019a31ec1
Janaina <janainasoul@icloud.com>
2023-02-21 18:54:02 -0300

    criando arquivo javascript com algumas informações

---
+++ gorjeta.js
@@ -0,0 +1,11 @@
+//constantes
+const inputContaTotal = document.querySelector(.contaTotal);
+const inputEscolhaValorGorjeta = document.querySelector(.gorjetaValor);
+const inputNumeroPessoas = document.querySelector(.numeroPessoas);
+const inputValorPorPessoa = document.querySelector(.valorPessoa);
+const inputTotalPorPessoa = document.querySelector(.totalPessoa);
+
+//variaveis
+let valorConta;
+let valorPessoas;
+let valorGorjeta;
