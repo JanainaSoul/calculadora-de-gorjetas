bfbd2e68169c31842c777deb7f38c6403ca6c70d
Janaina <janainasoul@icloud.com>
2023-02-24 16:26:40 -0300

    fazendo função do calculo total da conta

--- gorjeta.js
+++ gorjeta.js
@@ -1,11 +1,42 @@
 //constantes
-const inputContaTotal = document.querySelector(.contaTotal);
-const inputEscolhaValorGorjeta = document.querySelector(.gorjetaValor);
-const inputNumeroPessoas = document.querySelector(.numeroPessoas);
-const inputValorPorPessoa = document.querySelector(.valorPessoa);
-const inputTotalPorPessoa = document.querySelector(.totalPessoa);
+const contaTotal = document.querySelector('#contaTotal');
+const numeroPessoas = document.querySelector('#numeroPessoas');
+const escolhaValorGorjeta = document.querySelector('#gorjetaValor');
+const valorPessoa = document.querySelector('#valorPessoa');
+const totalPessoa = document.querySelector('#total');
+const limpar = document.querySelector('#limpar');
 
-//variaveis
-let valorConta;
-let valorPessoas;
-let valorGorjeta;
+
+// calculo da conta total
+let valorConta = '';
+ 
+contaTotal.addEventListener('input', (entrada) => {
+
+
+	if (entrada.data !== '0' && 
+		entrada.data !== '1' &&	
+		entrada.data !== null && 
+		entrada.data !== '2' && 
+		entrada.data !== '3' && 
+		entrada.data !== '4' && 
+		entrada.data !== '5' && 
+		entrada.data !== '6' && 
+		entrada.data !== '7' && 
+		entrada.data !== '8' && 
+		entrada.data !== '9' && 
+		entrada.data !== '.' && 
+		entrada.data !== ',') {
+		contaTotal.value = '';
+
+	}
+
+	if (contaTotal.value === '0') {
+		contaTotal.classList.add('erro');
+	}else {
+		contaTotal.classList.remove('erro');
+		valorConta = contaTotal.value;
+	}
+	
+	})
+
+	
